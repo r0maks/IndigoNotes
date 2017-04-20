bee99b7bae2d43c1910be641ce38ad6aa5f680a3
Román Maksimov <romaks@RM-MBP-07.local>
2017-04-20 11:55:39 -0400

    Initial Commit

---
+++ iPhoneProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhoneProject
+//
+//  Created by Román Maksimov on 4/20/17.
+//  Copyright © 2017 Román Maksimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
